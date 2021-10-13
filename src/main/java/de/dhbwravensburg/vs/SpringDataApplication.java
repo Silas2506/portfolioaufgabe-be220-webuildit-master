13678af5078f446660c91def21e98ff23b992908
Silas2506 <silas.blender@gmail.com>
2021-10-13 11:17:42 +0200

    Bitte funktionier

--- src/main/java/de/dhbwravensburg/vs/SpringDataApplication.java
+++ src/main/java/de/dhbwravensburg/vs/SpringDataApplication.java
@@ -9,7 +9,7 @@ public class SpringDataApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringDataApplication.class, args);
 
-		System.out.println("Dies ist ein Test");
+		System.out.println("Dies ist ein Test???!?!?!?");
 	}
 
 }
